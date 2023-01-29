f02344aa867dd13b450ed650f69660cf403efcd1
Yurik Galyaminsky <jobforandroid@proton.me>
2023-01-29 17:12:59 +0600

    Version 1.3 Release

--- app/src/main/java/me/proton/jobforandroid/compositionofnumbers/presentation/MainActivity.kt
+++ app/src/main/java/me/proton/jobforandroid/compositionofnumbers/presentation/MainActivity.kt
@@ -2,11 +2,18 @@ package me.proton.jobforandroid.compositionofnumbers.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import me.proton.jobforandroid.compositionofnumbers.R
+import com.google.android.gms.games.PlayGamesSdk
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var analytics: FirebaseAnalytics
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        analytics = Firebase.analytics
+        PlayGamesSdk.initialize(this)
     }
 }
--- app/src/main/java/me/proton/jobforandroid/compositionofnumbers/presentation/WelcomeFragment.kt
+++ app/src/main/java/me/proton/jobforandroid/compositionofnumbers/presentation/WelcomeFragment.kt
@@ -6,12 +6,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.games.PlayGamesSdk
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import me.proton.jobforandroid.compositionofnumbers.R
 import me.proton.jobforandroid.compositionofnumbers.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
     private var _binding: FragmentWelcomeBinding? = null
     private val binding get() = _binding!!
+    private lateinit var analytics: FirebaseAnalytics
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        analytics = Firebase.analytics
+        PlayGamesSdk.initialize(requireActivity())
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
